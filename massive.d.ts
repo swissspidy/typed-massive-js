cd84eec16f1b829886432bc116ac0cd93881fb20
Pascal Birchler <hello@pascalbirchler.ch>
2016-04-05 09:30:15 +0200

    Add namespace

--- massive.d.ts
+++ massive.d.ts
@@ -4,64 +4,69 @@
 // Definitions: https://github.com/swissspidy/typed-massive-js
 
 declare module massive {
-  interface IConnectionOptions {
-    connectionString?: string;
-    db?: string;
+  namespace massive {
+    interface IConnectionOptions {
+      connectionString?: string;
+      db?: string;
+    }
+
+    export interface ITable {
+      find(context: any, callback: (err, res) => {}): void;
+      find(context: any,
+         options: {
+           limit?: number;
+           order?: string;
+           offset?: number;
+           columns?: string[];
+         },
+         callback: (err, res) => {}): void;
+      findOne(context: any, callback: (err, res) => {}): void;
+      findOne(context: any,
+          options: {
+            limit?: number;
+            order?: string;
+            offset?: number;
+            columns?: string[];
+          },
+          callback: (err, res) => {}): void;
+
+      insert(context: any, callback: (err, res) => {}): void;
+      save(context: any, callback: (err, res) => {}): void;
+      update(context: any, callback: (err, res) => {}): void;
+      update(context: any, data: any, callback: (err, res) => {}): void;
+      destroy(context: any, callback: (err, res) => {}): void;
+
+    }
+
+    interface IDoc {
+      findDoc(context: any, callback: (err, res) => {}): void;
+      searchDoc(options: {
+        keys: string[];
+        term: string;
+      }, callback: (err, res) => {})
+      saveDoc(context: string, callback: (err, res) => {}): void;
+      destroy(context: any, callback: (err, res) => {}): void;
+    }
+
+    export interface ITables {
+      comment: ITable;
+      friendship: ITable;
+      participation: ITable;
+      person: ITable;
+      practice: ITable;
+      practicesession: ITable;
+      sport: ITable;
+      team: ITable;
+      teammember: ITable;
+      teamsport: ITable;
+    }
+
+    export function connectSync(options: IConnectionOptions): ITables
+
+    export function connect(options: IConnectionOptions, callback: (err, db) => {});
+
+    export function run(context: string, filter: number|number[], callback: (err, res) => {})
+
+    export function saveDoc(name: string, data: any, callback: (err, res) => {}): void;
   }
-
-  export interface ITable {
-    find(context: any, callback: (err, res) => {}): void;
-    find(context: any,
-        options: {
-          limit?: number;
-          order?: string;
-          offset?: number;
-          columns?: string[];
-        },
-        callback: (err, res) => {}): void;
-    findOne(context: any, callback: (err, res) => {}): void;
-    findOne(context: any,
-            options: {
-              limit?: number;
-              order?: string;
-              offset?: number;
-              columns?: string[];
-            },
-            callback: (err, res) => {}): void;
-
-    insert(context: any, callback: (err, res) => {}): void;
-    save(context: any, callback: (err, res) => {}): void;
-    update(context: any, callback: (err, res) => {}): void;
-    update(context: any, data: any, callback: (err, res) => {}): void;
-    destroy(context: any, callback: (err, res) => {}): void;
-
-  }
-
-  interface IDoc {
-    findDoc(context: any, callback: (err, res) => {}): void;
-    searchDoc(options: {
-                keys: string[];
-                term: string;
-              }, callback: (err, res) => {})
-    saveDoc(context: string, callback: (err, res) => {}): void;
-    destroy(context: any, callback: (err, res) => {}): void;
-  }
-
-  export interface ITables {
-    comment: ITable;
-    friendship: ITable;
-    participation: ITable;
-    person: ITable;
-    practice: ITable;
-    practicesession: ITable;
-    sport: ITable;
-    team: ITable;
-    teammember: ITable;
-    teamsport: ITable;
-  }
-
-  export function connectSync(options: IConnectionOptions): ITables
-  export function connect(options: IConnectionOptions, callback: (err, db) => {});
-  export function run(context: string, filter: number|number[], callback: (err, res) => {})
-  export function saveDoc(name: string, data: any, callback: (err, res) => {}): void;
 }
